ed81f3d43d16d97e559cca84c7a124e66ed08140
Frederick Mill <151436762+freddypm55@users.noreply.github.com>
2024-03-23 07:19:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,18 +20,18 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '2349031616504' //ur owner number
+global.ownername = '🦄𒄬𒄬𒄬𒄬ᖴᖇEᗪEᖇIᑕK࿊' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.wagc = "https://whatsapp.com/channe"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.botscript = 'https://github.com/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.author = "🦄드림 가이 Xeon\n\n+2349031616504"
+global.creator = "2349031616504@s.whatsapp.net"
+global.xprefix = ','
+global.premium = ["2349031616504"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -42,8 +42,8 @@ global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.adminevent = true //show promote/demote message
+global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
